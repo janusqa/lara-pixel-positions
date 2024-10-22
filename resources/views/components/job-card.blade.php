6961d4840ae8dcb78303188151d92203936cd24e
Janus Developer <dev@retrievo.net>
2024-10-22 09:53:24 -0400

    layout part 2

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,15 +1,15 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 font-bold text-xl transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag href="#">Tag</x-tag>
-            <x-tag href="#">Tag</x-tag>
-            <x-tag href="#">Tag</x-tag>
+            <x-tag href="#" size="small">Backend</x-tag>
+            <x-tag href="#" size="small">Frontend</x-tag>
+            <x-tag href="#" size="small">Manager</x-tag>
         </div>
-        <img src="https://placehold.it/42/42" alt="logo" class="rounded-xl">
+        <x-employer-logo :width="42" />
     </div>
-</div>
+</x-panel>
